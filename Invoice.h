ecf87a3715d12bffbc4909fc865bc380672f3377
Humberto Navarro <54050212+humbertonc@users.noreply.github.com>
2019-12-05 10:24:55 -0300

    Create Invoice.h

---
+++ Invoice.h
@@ -0,0 +1,33 @@
+#ifndef INVOICE_H
+#define INVOICE_H
+#include <string>
+
+using namespace std;
+class Invoice
+{
+    public:
+
+        Invoice(int num, int qtd, string desc, double p);
+
+        virtual ~Invoice();
+
+        int getNumero();
+        void setNumero(int num);
+        string getDesc();
+        void setDesc(string des);
+        int getQtd();
+        void setQtd(int qtd);
+        double getPreco();
+        void setPreco(double p);
+
+        double getInvoiceAmount(int qtd, double p);
+
+    protected:
+
+    private:
+        string descricao;
+        int numero, quantidade;
+        double preco;
+};
+
+#endif
